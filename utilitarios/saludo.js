1e117fda2443ae3fb6b591b6464e8111151ea9c5
drCupueran <diegorcg16@gmail.com>
2025-09-30 20:28:25 -0500

    funcion mostrarTexto implementada

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -5,6 +5,13 @@ saludar = function () {
     let apellido = recuperarTexto("txtApellido");
     let edad = recuperarInt("txtEdad");
     let estatura = recuperarFloat("txtEstatura");
+    let mensajeBienvenida = "Bienvenido " + nombre + " " + apellido;
+    mostrarTexto("lblResultado", mensajeBienvenida);
+}
+mostrarTexto = function (idComponente, mensaje) {
+    let componente = document.getElementById(idComponente);
+    componente.innerText = mensaje;
+       
 }
 recuperarTexto = function (idComponente) {
     let componente = document.getElementById(idComponente);
