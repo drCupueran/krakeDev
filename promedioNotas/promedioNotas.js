c2dcf03bf8b1da3a733a39cb4c4fc2b9e7133021
drCupueran <diegorcg16@gmail.com>
2025-10-03 22:58:45 -0500

    sentencia if y else agregado, ademas cambio de imagen

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -5,5 +5,11 @@ calcularPromedioNotas = function () {
 
     let promedio = calcularPromedio(nota1, nota2, nota3);
     cambiarTexto("lblPromedio", promedio.toFixed(2));
+    
+    if (promedio > 7) {
+        cambiarImagen("imgResultado", "./imagenes/apro.gif"); // Ruta del gif de éxito
+    } else {
+        cambiarImagen("imgResultado", "./imagenes/repro.gif"); // Ruta del gif de fracaso
+    }
     return promedio;
 }
