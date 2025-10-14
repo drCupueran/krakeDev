ac614a7a0330a4ffd531534086f8beb5d1f2c1b5
drCupueran <diegorcg16@gmail.com>
2025-10-13 20:38:59 -0500

    Cambioe en promedio notas, adicionando operadores logicos,

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -4,12 +4,18 @@ calcularPromedioNotas = function () {
     let nota3 = recuperarFlotante("txtNota3");
 
     let promedio = calcularPromedio(nota1, nota2, nota3);
-    cambiarTexto("lblPromedio", promedio.toFixed(2));
     
-    if (promedio > 7) {
-        cambiarImagen("imgResultado", "./imagenes/apro.gif"); // Ruta del gif de éxito
+    if (promedio < 5  && promedio > 0) {
+        cambiarImagen("imgResultado", "./imagenes/repro.gif"); // Ruta del gif de reprobado
+        cambiarTexto("lblPromedio", promedio.toFixed(2)+" Reprobado");
+    } else if (promedio >= 5 && promedio <= 8) {
+        cambiarImagen("imgResultado", "./imagenes/mucho.gif"); // Ruta del gif de normal
+        cambiarTexto("lblPromedio",promedio.toFixed(2)+" Buen Trabajo");
+    }else if (promedio > 8 && promedio <= 10) {
+        cambiarImagen("imgResultado", "./imagenes/apro.gif"); // Ruta del gif de aprobado
+        cambiarTexto("lblPromedio",promedio.toFixed(2)+ " Aprobado");
     } else {
-        cambiarImagen("imgResultado", "./imagenes/repro.gif"); // Ruta del gif de fracaso
+        cambiarImagen("imgResultado", "./imagenes/error.gif"); // Si el promedio no es válido, muestra un gif de error
+        cambiarTexto("lblPromedio", "Datos Incorrectos");
     }
-    return promedio;
-}
+   }
