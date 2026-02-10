0e1fd0a054d4660238bc14e54f96728866b0a6f0
drCupueran <diegorcg16@gmail.com>
2026-02-09 21:32:48 -0500

    validacion del ganador cuando tenga 5 victorias

--- Juego/juego.js
+++ Juego/juego.js
@@ -5,19 +5,35 @@ jugar = function (seleccionado) {
  
     let elemento = generarElemento();
     let ruta = generarRuta(elemento);
+    mostrarImagen("imgComputadora", ruta);
+
     let ganador = determinarGanador(seleccionado, elemento);
 
     if (ganador == 0) {
-        mostrarTexto("EMPATE");
+        mostrarTexto("txtResultado", "EMPATE");
     } else if (ganador == 1) {
-        mostrarTexto("GANASTE LA PARTIDA");
-        puntosUsuario= puntosUsuario + 1;
+        mostrarTexto("txtResultado", "GANASTE LA PARTIDA");
+        puntosUsuario++;
         
     } else {
-        mostrarTexto("PERDISTE LA PARTIDA");
-        puntosComputadora = puntosComputadora + 1;
-        
+        mostrarTexto("txtResultado", "PERDISTE LA PARTIDA");
+        puntosComputadora++;
     }
+    mostrarTexto("puntosUsuario", puntosUsuario);
+    mostrarTexto("puntosComputadora", puntosComputadora);
+    validarSeleccion(puntosUsuario, puntosComputadora);
 
 }
 
+validarSeleccion = function (puntosUsuario, puntosComputadora) {
+    if (puntosUsuario == 5) {
+        mostrarTexto("txtResultado", "HAS GANDO EL JUEGO");
+        puntosUsuario = 0;
+        puntosComputadora = 0;
+    } else if (puntosComputadora == 5) {
+        mostrarTexto("txtResultado", "EL COMPUTADOR TE HA VENCIDO");
+        puntosUsuario = 0;
+        puntosComputadora = 0;
+    }
+    return puntosUsuario, puntosComputadora;
+}
